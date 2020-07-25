7b7d4e2fd585503ae0965bccb6d875c1d6773f6e
HawksJamesf <Hawks.Jamesf@gmail.com>
2020-07-25 09:05:18 +0800

    add compose ui
    Signed-off-by: HawksJamesf <Hawks.Jamesf@gmail.com>

---
+++ app/src/main/java/com/hawksjamesf/spacecraft/compose_ui.kt
@@ -0,0 +1,20 @@
+//@Composable
+//fun TutorialPreviewTemplate(
+//        colors: MaterialColors = lightThemeColors,
+//        typography: MaterialTypography = themeTypography
+//) {
+//    val context = +ambient(ContextAmbient)
+//    val previewPosts = getPostsWithImagesLoaded(posts.subList(1, 2), context.resources)
+//    val post = previewPosts[0]
+//    MaterialTheme(colors = colors, typography = typography) {
+//        Surface {
+//            PostCardTop(post)
+//        }
+//    }
+//}
+//@Composable
+//fun TutorialPreview() {
+//    // Call the composable function that you would like to
+//    // create a live preview for.
+//    TutorialPreviewTemplate()
+//}
